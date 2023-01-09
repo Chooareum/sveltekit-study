8ea6eee5305167b7a739d8f4d7493c23abf42518
추아름 <choo.areum@mintech.kr>
2023-01-09 14:05:48 +0900

    first commit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  let name = '알미'
+</script>
+
+<h1>{name} 홈페이지</h1>
+
+<style>
+  h1 {
+    font-size: 30px;
+  }
+</style>
---
+++ my-app/src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let data;
+</script>
+
+<form method="POST" action="/abc">
+  <input name="email" />
+  <input name="password" />
+  <button>로그인</button>
+</form>
+
+<h1>about page {data.title}</h1>
